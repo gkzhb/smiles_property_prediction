1d7ef66e97c3d9be90402e5e6f42a34ac25dbdd0
zhb <zhb896579388@163.com>
2020-07-08 12:38:27 +0800

    Add mole2vec.py(using mol2vec)

---
+++ mole2vec.py
@@ -0,0 +1,14 @@
+from mol2vec import features
+from gensim.models import word2vec
+from rdkit import Chem
+
+def mole2vec(mols: list):
+    """
+    mols: list of str(SMILES)
+    return: list of np.array
+    """
+    model = word2vec.Word2Vec.load('./model/model_300dim.pkl')
+    rd_mol = [Chem.MolFromSmiles(i) for i in mols]
+    sentences = [features.MolSentence(features.mol2alt_sentence(i, 1)) for i in rd_mol]
+    vecs = [features.sentences2vec(i, model, unseen='UNK') for i in sentences]
+    return vecs
